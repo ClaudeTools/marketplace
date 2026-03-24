0fb81617a59d48791794346a1921aa55c748daf4
owenob1 <me@oweninnes.com>
2026-03-24 19:48:16 +1100

    chore: remove project-map.d.ts

--- plugins/claudetools/codebase-pilot/dist/project-map.d.ts
+++
@@ -1,3 +0,0 @@
-import Database from "better-sqlite3";
-export declare function generateProjectMap(projectRoot: string, existingDb?: Database.Database): string;
-//# sourceMappingURL=project-map.d.ts.map
